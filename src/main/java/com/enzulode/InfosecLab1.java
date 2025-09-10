5f3eb3235ac37de82c19a51b01c668f9fb91ceb8
Vladislav Andreev <andreevvlad443@gmail.com>
2025-09-10 18:02:40 +0300

    initial commit

---
+++ src/main/java/com/enzulode/InfosecLab1.java
@@ -0,0 +1,12 @@
+package com.enzulode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosecLab1 {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InfosecLab1.class, args);
+  }
+}
