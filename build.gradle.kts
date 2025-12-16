a4b4d85b3c9d65ffb3081ef6451ca1e6c4691129
Vladislav Andreev <andreevvlad443@gmail.com>
2025-12-16 23:26:42 +0300

    feat: spotbugs & snyk initial configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.SpotBugsTask
+
 plugins {
     java
     alias(libs.plugins.springBoot)
     alias(libs.plugins.springDependencyManagement)
+    id("com.github.spotbugs") version("6.4.8") // SAST
 }
 
 group = "com.enzulode"
@@ -43,4 +48,32 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+spotbugs {
+    toolVersion = "4.9.8"
+    ignoreFailures = false
+    effort = Effort.MAX
+    reportLevel = Confidence.MEDIUM
+}
+
+tasks.withType<SpotBugsTask> {
+    reports {
+        create("xml") {
+            enabled = true
+            outputLocation = layout.buildDirectory.get().dir("reports/spotbugs/spotbugs-report.xml").asFile
+        }
+        create("sarif") {
+            enabled = true
+            outputLocation = layout.buildDirectory.get().dir("reports/spotbugs/spotbugs-report.sarif").asFile
+        }
+        create("html") {
+            enabled = true
+            outputLocation = layout.buildDirectory.get().dir("reports/spotbugs/spotbugs-report.html").asFile
+        }
+    }
+}
+
+tasks.named<DefaultTask>("check") {
+    dependsOn(tasks.withType<SpotBugsTask>())
 }
